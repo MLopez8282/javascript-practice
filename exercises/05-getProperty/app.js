66354a97c9ed50efb34f3066a2662bd9a97cc421
MLopez8282 <saltairpix@gmail.com>
2020-10-22 00:36:14 +0000

    practic

--- exercises/05-getProperty/app.js
+++ exercises/05-getProperty/app.js
@@ -1,3 +1,9 @@
 function getProperty(obj, key) {
   // your code here
+  
+  var car = {
+      model: 'Toyota'
+  }
 }
+var output = getProperty(car, 'Toyota')
+console.log(output)
--- exercises/08-checkAge/app.js
+++ exercises/08-checkAge/app.js
@@ -1,3 +1,10 @@
 function checkAge(name, age) {
   // your code here
+  if (age < 21){
+      return `Go home, ${name}`;
+  }
+  else if (age >= 21){
+      return `Welcome, ${name}`;
+  }
 }
+console.log(checkAge('Adrian',13))
